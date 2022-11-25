28ae49273322ad659c60c36566b22ede998739c8
thanawat-bcr <thanawat.bcr@gmail.com>
2022-11-25 11:35:29 +0700

    feat: json formatter

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -3,6 +3,8 @@ import Axios from "axios";
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 
+import JsonFormatter from 'react-json-formatter';
+
 function Post(props) {
   const url = "http://localhost:8080/product";
   //change url
@@ -20,10 +22,7 @@ function Post(props) {
     products: [],
     mstamp: "",
     isApplyAccum: "",
-    coupon: {
-      barcode: "",
-      quantity: "",
-    },
+    coupon: [],
     tender: {
       tenderno: "",
       amount: "",
@@ -32,9 +31,29 @@ function Post(props) {
       id: "",
       earn: "",
     },
-    localSegments: { segId: "" },
   });
-
+  // const [data, setData] = useState({
+  //   cid: "7DELIVERY",
+  //   mode: "submit",
+  //   member_id: "0876191414",
+  //   storeid: "09988",
+  //   posid: "99",
+  //   inv: "701172",
+  //   saledate: "2022-11-18",
+  //   products: [],
+  //   mstamp: "",
+  //   isApplyAccum: "",
+  //   coupon: [],
+  //   tender: {
+  //     tenderno: "000",
+  //     amount: 0,
+  //   },
+  //   redeems: {
+  //     id: "809",
+  //     earn: 30,
+  //   },
+  // });
+  
   const cidOptions = [
     { value: "7DELIVERY", label: "7DELIVERY" },
     { value: "POSONL", label: "POSONL" },
@@ -61,8 +80,6 @@ function Post(props) {
       newdata.redeems.id = e.value;
     } else if (e.id === "EARN") {
       newdata.redeems.earn = e.value;
-    } else if (e.id === "SEGMENT") {
-      newdata.localSegments.segId = e.value;
     } else if (e.id === "cid") {
       if (e.label === "7DELIVERY") {
         setSegId("106000003");
@@ -138,7 +155,7 @@ function Post(props) {
     temp.products = productArray;
     temp.mstamp = temp.mstamp === "true";
     temp.isApplyAccum = temp.isApplyAccum === "true";
-    temp.localSegments.segId = segId;
+    temp.redeems = temp.redeems.id ? temp.redeems : [];
     temp.saledate = temp.saledate.concat(" 00:00:00");
     console.log('temp', temp)
     Axios.post(url, temp).then((res) => {
@@ -148,6 +165,16 @@ function Post(props) {
     // console.log(temp);
     // console.log(productArray);
   }
+
+  // COPY HANDLER
+
+function copyHandler() {
+  // var copyText = JSON.stringify(json);
+  // copyText.select();
+  // copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(JSON.stringify(json));
+  alert("Copied to your clipboard!")
+}
 
   function reset() {
     setJson(null)
@@ -162,10 +189,7 @@ function Post(props) {
       products: [],
       mstamp: "",
       isApplyAccum: "",
-      coupon: {
-        barcode: "",
-        quantity: "",
-      },
+      coupon: [],
       tender: {
         tenderno: "",
         amount: "",
@@ -174,20 +198,29 @@ function Post(props) {
         id: "",
         earn: "",
       },
-      localSegments: { segId: "" },
     })
   }
+
+  const jsonStyle = {
+    propertyStyle: { color: 'green' },
+    stringStyle: { color: '#c92727' },
+    numberStyle: { color: 'purple' },
+    booleanStyle: { color: '#c92727' },
+  }
+
   return (
     <>
-      <div className='title'>JSON Request Generator for UAT</div>
+      <div className='title'>JSON Request Generator</div>
       <div className="container">
         {
           json ?
           (
-            <>
-              <div style={{ wordWrap: "break-word"}}>{JSON.stringify(json)}</div>
+            <div className="json-formatted">
+              {/* <div style={{ wordWrap: "break-word"}}>{JSON.stringify(json)}</div> */}
+              <JsonFormatter json={JSON.stringify(json)} tabWith={4} jsonStyle={jsonStyle} />
+              <button className='block-button outline' id='copy-button' onClick={copyHandler}>Copy to Clipboard</button>
               <button className='block-button' onClick={reset}>Clear</button>
-            </>
+            </div>
           ) :
           (
             <form onSubmit={(e) => submit(e)}>
@@ -250,7 +283,7 @@ function Post(props) {
             type="text"
           />
 
-          <p className="header">INV</p>
+          <p className="header">Invoice No.</p>
           <input
             onChange={(e) => handle(e.target)}
             id="inv"
@@ -259,49 +292,45 @@ function Post(props) {
             type="text"
           />
 
-          <div>
-            <p className="header">Sale Date</p>
-            <input
-              onChange={(e) => handle(e.target)}
-              id="saledate"
-              value={data.saledate}
-              placeholder="Sale Date (yyyy-mm-dd)"
-              type="text"
-            />
-          </div>
+          <p className="header">Sale Date</p>
+          <input
+            onChange={(e) => handle(e.target)}
+            id="saledate"
+            value={data.saledate}
+            placeholder="Sale Date (yyyy-mm-dd)"
+            type="text"
+          />
 
-          <div>
-            <div className="product-list">
-              <p className="header">Products List</p>
-              <div className='button' onClick={() => productAddHandler()}>Add a product</div>
-            </div>
-            {
-              productArray.length > 0 ?
-                productArray.map((product, i) => {
-                return (
-                  <div key={i} style={{marginTop: "0.75rem"}}>
-                    <div style={{display: "flex", gap: "8px"}}>
-                      <input
-                        id="code"
-                        onChange={(e) => productHandler(e, i)}
-                        type="text"
-                        placeholder="Product Code"
-                      />
-                      <input
-                        id="quantity"
-                        onChange={(e) => productHandler(e, i)}
-                        type="number"
-                        placeholder="Quantity"
-                      />
-                      <div className='close-button' onClick={() => productDeleteHandler(i)}></ div>
-                    </div>
-                  </div>
-                );
-              }) :
-              <div className='empty-state' onClick={() => productAddHandler()}
-              >There is no product yet, please add more.</div>
-            }
+          <div className="product-list">
+            <p className="header">Products List</p>
+            <div className='button' onClick={() => productAddHandler()}>Add a product</div>
           </div>
+          {
+            productArray.length > 0 ?
+              productArray.map((product, i) => {
+              return (
+                <div key={i} style={{marginBottom: "0.75rem"}}>
+                  <div style={{display: "flex", gap: "8px"}}>
+                    <input
+                      id="code"
+                      onChange={(e) => productHandler(e, i)}
+                      type="text"
+                      placeholder="Product Code"
+                    />
+                    <input
+                      id="quantity"
+                      onChange={(e) => productHandler(e, i)}
+                      type="number"
+                      placeholder="Quantity"
+                    />
+                    <div className='close-button' onClick={() => productDeleteHandler(i)}></ div>
+                  </div>
+                </div>
+              );
+            }) :
+            <div className='empty-state' onClick={() => productAddHandler()}
+            >There is no product yet, please add more.</div>
+          }
 
           <p className="header">M Stamp</p>
           <Dropdown
@@ -341,38 +370,36 @@ function Post(props) {
             type="text"
           />
 
-          <div>
-            <div className="product-list">
-              <p className="header">Coupon List</p>
-              <div className='button' onClick={() => couponAddHandler()}>Add a Coupon</div>
-            </div>
-            {
-                couponArray.length > 0 ?
-                  couponArray.map((coupon, i) => {
-                    return (
-                      <div key={i} style={{marginTop: "0.75rem"}}>
-                        <div style={{display: "flex", gap: "8px"}}>
-                          <input
-                            id="barcode"
-                            onChange={(e) => couponHandler(e, i)}
-                            type="text"
-                            placeholder="Coupon Barcode"
-                          />
-                          <input
-                            id="quantity"
-                            onChange={(e) => couponHandler(e, i)}
-                            type="number"
-                            placeholder="Coupon Quantity"
-                          />
-                          <div className='close-button' onClick={() => couponDeleteHandler(i)}></ div>
-                        </div>
-                      </div>
-                    );
-                  }) :
-                  <div className='empty-state' onClick={() => couponAddHandler()}
-                  >There is no coupon yet, please add more.</div>
-            }
+          <div className="product-list">
+            <p className="header">Coupon List</p>
+            <div className='button' onClick={() => couponAddHandler()}>Add a Coupon</div>
           </div>
+          {
+              couponArray.length > 0 ?
+                couponArray.map((coupon, i) => {
+                  return (
+                    <div key={i} style={{marginBottom: "0.75rem"}}>
+                      <div style={{display: "flex", gap: "8px"}}>
+                        <input
+                          id="barcode"
+                          onChange={(e) => couponHandler(e, i)}
+                          type="text"
+                          placeholder="Coupon Barcode"
+                        />
+                        <input
+                          id="quantity"
+                          onChange={(e) => couponHandler(e, i)}
+                          type="number"
+                          placeholder="Coupon Quantity"
+                        />
+                        <div className='close-button' onClick={() => couponDeleteHandler(i)}></ div>
+                      </div>
+                    </div>
+                  );
+                }) :
+                <div className='empty-state' onClick={() => couponAddHandler()}
+                >There is no coupon yet, please add more.</div>
+          }
 
           <p className="header">Tender</p>
           <div  style={{display: "flex", gap: "8px"}}>
@@ -409,11 +436,8 @@ function Post(props) {
               type="text"
             />
           </div>
-          {/* <div>
-            <p className="header">Local Segments</p>
-            <input disabled value={segId} />
-          </div> */}
-          <button className='block-button'>Generate</button>
+
+          <button className='block-button' style={{marginTop: '1.5rem', marginBottom: '1rem'}}>Generate</button>
         </form>
           )
         }
